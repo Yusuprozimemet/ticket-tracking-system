ac3d4639f85b55ed4c0e1dedae471900fe83f45e
Yusuprozimemet <yusuf.rozimemet@gmail.com>
2026-06-15 20:33:58 +0200

    feat: scaffold email notifications via Resend

--- src/main/java/net/hackyourfuture/tickettrackingsystem/config/RestClientConfig.java
+++ src/main/java/net/hackyourfuture/tickettrackingsystem/config/RestClientConfig.java
@@ -1,5 +1,20 @@
 package net.hackyourfuture.tickettrackingsystem.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestClient;
+
+@Configuration
 public class RestClientConfig {
+
+    @Bean
+    public RestClient resendRestClient(RestClient.Builder builder,@Value("${resend.api-key:}") String apiKey) {
+        return builder
+                .baseUrl("https://api.resend.com")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                .build();
+    }
     
 }
